a4e25ab152bcdfd573c853160924f22d0ae0022f
code2college <code2college@code2colegesMBP.attlocal.net>
2023-04-28 13:35:33 -0500

    Title and resizing

--- main.py
+++ main.py
@@ -1,139 +1,290 @@
 import tkinter as tk
 import pymysql
 
-
+root = tk.Tk()
 con = pymysql.Connect(
     host='localhost',
     port=3306,
     user='root',
     password='Neha_41s',
     db='Bank Details',
-    charset='utf8'
-)
+    charset='utf8')
 
 cur = con.cursor()
 
-"""
-If 1; then ask for bank:  photo ID, birthdate, SSN, an inital deposit amount, 
-phone number
-If 2; ask for username and password,then ask if check balance, deposit, 
-withdraw, then ask for amount
-If 3; sign an account closing form,
-If 4; provide them all their information and ask which part needs to be 
-modified(edit name, PIN, or any other personal identification required to 
-open an account)
-"""
-print("\n\n Hello! Thank you for choosing our bank!")
+name_var=tk.StringVar()
+username_var=tk.StringVar()
+passw_var=tk.StringVar()
+initialamt_var=tk.StringVar()
+what_var = tk.StringVar()
+to_what_var=tk.StringVar()
+sure_var=tk.StringVar()
+choice_var=tk.StringVar()
+
+root.title("Banking System")
+root.geometry("600x400")
+
+def submit():
+    choice = choice_var.get()
+    print("The choice is : " + choice)
+    choice_var.set("")
+
+    if choice == "1":
+        enter_info_for_create() 
+        root.winfo_children()[1]
+        
+    elif choice == "2":
+        enter_info_for_login()
+        root.winfo_children()[0]
+
+    elif choice == "3":
+        enter_info_for_close()
+        root.winfo_children()[0]
+
+    elif choice == "4":
+        enter_info_for_modify()
+        root.winfo_children()[1]
+
+    elif choice == "5":
+        exit()
+        exit
+
 def menu():
-    return """
-    Your options are:
-    1) Create a new account
-    2) Login to account 
-    3) Close account
-    4) Modify existing account
-    5) Exit
-     """
+    menu1= tk.Label(root, text = "Your options are:")
+    menu2= tk.Label(root, text = "1) Create a new account")
+    menu3= tk.Label(root, text = "2) Login to account")
+    menu4= tk.Label(root, text = "3) Close account")
+    menu5= tk.Label(root, text = "4) Modify existing account")
+    menu6= tk.Label(root, text = "5) Exit")
+    choice_label = tk.Label(root, text = 'Enter your choice(in number):', font=('calibre',10, 'normal'))
+    choice_entry = tk.Entry(root, textvariable = choice_var, font=('calibre',10,'normal'))
+    sub_btn=tk.Button(root,text = 'Submit', command = submit)
+
+    menu1.grid()
+    menu2.grid()
+    menu3.grid()
+    menu4.grid()
+    menu5.grid()
+    menu6.grid()
+    choice_label.grid()
+    choice_entry.grid()
+    sub_btn.grid()
+
 
 
 def create_account():
-    name = input("Enter your name: ")
-    username = input("Enter a username(no special characters): ")
-    password = input("Enter a password(no special characters): ")
-    balance = int(input("Enter an amount for inital deposit: $ "))
+ 
+    name=name_var.get()
+    username = username_var.get()
+    password=passw_var.get()
+    balance = initialamt_var.get()
+    print("The name is : " + name)
+    print("The username is:" + username)
+    print("The password is : " + password)
+    print("The initial amount is: $" + balance)
+     
+    name_var.set("")
+    username_var.set("")
+    passw_var.set("")
+    initialamt_var.set("")
+
     sql4 = "INSERT INTO Customer_Details (Name, Username, Password, Balance) VALUES(%s,%s,%s,%s)"
     val = (name, username, password, balance)
     cur.execute(sql4, val)
     con.commit()
-    return "A new account was created for you!"    
+    ret = tk.Label(root, text= "Account was successfully created!")
+    ret.grid()
 
+def enter_info_for_create():
+    label= tk.Label(root, text = "Creating a New Account- No Special characters")
+    name_label = tk.Label(root, text = 'Name', font=('calibre',10, 'normal'))
+    name_entry = tk.Entry(root, textvariable = name_var, font=('calibre',10,'normal'))
+    username_label = tk.Label(root, text = 'Username', font=('calibre',10, 'normal'))
+    username_entry=tk.Entry(root, textvariable = username_var, font = ('calibre',10,'normal'))
+    passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'normal'))
+    passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
+    initialamt_label = tk.Label(root, text= 'Enter inital amount',font=('calibre',10, 'normal'))
+    initialamt_entry=tk.Entry(root, textvariable = initialamt_var, font = ('calibre',10,'normal'))
+    sub_btn=tk.Button(root,text = 'Submit', command = create_account)
 
+    label.grid()
+    name_label.grid()
+    name_entry.grid()
+    username_label.grid()
+    username_entry.grid()
+    passw_label.grid()
+    passw_entry.grid()
+    initialamt_label.grid()
+    initialamt_entry.grid()
+
+    sub_btn.grid()
 
 def login():
-    user = input("Please enter your username: ")
-    password1 = input("Please enter your password: ")
+    username = username_var.get()
+    password=passw_var.get()
+    print("The username is:" + username)
+    print("The password is : " + password)
+    username_var.set("")
+    passw_var.set("")
     sql1 = 'SELECT * FROM Customer_Details'
     cur.execute(sql1)
     rows = cur.fetchall()
-    result = any(user in tu for tu in rows)
-    result1 = any(password1 in tu for tu in rows)
+    result = any(username in tu for tu in rows)
+    result1 = any(password in tu for tu in rows)
     con.commit()
     if result == True and result1 == True:
-        print('Login Successful!')
+       ret= tk.Label(root, text='Login Successful!')
+       ret.grid()
     else:
-        print("Your username or password is wrong. Please try again!")
+        ret=tk.Label(root, text="Your username or password is wrong. Please try again!")
+        ret.grid()
+
+
+def enter_info_for_login():
+    label = tk.Label(root, text= "Logining In-")
+    username_label = tk.Label(root, text = 'Username', font=('calibre',10, 'normal'))
+    username_entry=tk.Entry(root, textvariable = username_var, font = ('calibre',10,'normal'))
+    passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'normal'))
+    passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
+
+    sub_btn=tk.Button(root,text = 'Submit', command = login)
+
+    label.grid()
+    username_label.grid()
+    username_entry.grid()
+    passw_label.grid()
+    passw_entry.grid()
+    sub_btn.grid()
+
+def modify():
+    username = username_var.get()
+    password=passw_var.get()
+    what = what_var.get().lower()
+    to_what = to_what_var.get()
+    print("The username is : " + username)
+    print("The password is:" + password)
+    print("The what is : " + what)
+    print("The to what is:" + to_what)
+
+    username_var.set("")
+    passw_var.set("")
+    what_var.set("")
+    to_what_var.set("")
+
+    sql1 = 'SELECT * FROM Customer_Details'
+    cur.execute(sql1)
+    rows = cur.fetchall()
+    result = any(username in tu for tu in rows)
+    result1 = any(password in tu for tu in rows)
+    print(f"Result is: {result}, Result1 is: {result1}")
+    con.commit()
+    if result == True and result1 == True:
+        new = to_what
+        if what == 'name':
+            command = 'UPDATE Customer_Details SET Name = %s WHERE Username = %s'
+            val = (new, username)
+            cur.execute(command, val)
+            con.commit()
+            ret = tk.Label(root, text="Name was modified")
+            ret.grid()
+        if what == 'username':
+            command = 'UPDATE Customer_Details SET Username = %s WHERE Username = %s'
+            val = (new, username)
+            cur.execute(command, val)
+            con.commit()
+            ret = tk.Label(root, text="Username was modified")
+            ret.grid()
+        if what == 'password':
+            command = 'UPDATE Customer_Details SET password = %s WHERE Username = %s'
+            val = (new, username)
+            cur.execute(command, val)
+            con.commit()
+            ret = tk.Label(root, text="Password was modified")
+            ret.grid()
+        else:
+            ret= tk.Label(root, text="The choice you entered is not correct!")
+            ret.grid()
+    else:
+        ret= tk.Label(root, text="Something went wrong! Please make sure you entered your Username and Password correctly.")
+        ret.grid()
+
+def enter_info_for_modify():
+    label = tk.Label(root, text= "Modifying Account-")
+    username_label = tk.Label(root, text = 'Username', font=('calibre',10, 'normal'))
+    username_entry=tk.Entry(root, textvariable = username_var, font = ('calibre',10,'normal'))
+    passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'normal'))
+    passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
+    what_label = tk.Label(root, text = 'What would you like to modify in your account(name, username, or password)?', font=('calibre',10, 'normal'))
+    what_entry=tk.Entry(root, textvariable = what_var, font = ('calibre',10,'normal'))
+    to_what_label=tk.Label(root, text = 'What do you want to change it to?', font=('calibre',10, 'normal'))
+    to_what_entry=tk.Entry(root, textvariable = to_what_var, font = ('calibre',10,'normal'))
+    
+    sub_btn=tk.Button(root,text = 'Submit', command = modify)
+    
+    label.grid()
+    username_label.grid()
+    username_entry.grid()
+    passw_label.grid()
+    passw_entry.grid()
+    what_entry.grid()
+    what_label.grid()
+    to_what_label.grid()
+    to_what_entry.grid()
+    sub_btn.grid()
 
 
 def close():
-    sure = input("Are you sure you want to close your account? ").lower()
+    sure = sure_var.get().lower()
+    username = username_var.get()
+    password=passw_var.get()
+    print("The sure is : " + sure)
+    print("The username is:" + username)
+    print("The password is : " + password)
+    sure_var.set("")
+    username_var.set("")
+    passw_var.set("")
     if sure == 'yes':
-        username1 = input("Please enter your username: ")
-        password2 = input("Please enter your password: ")
         sql3 = "delete from Customer_Details where username = %s and password = %s"
-        val = (username1, password2)
+        val = (username, password)
         cur.execute(sql3, val)
         con.commit()
+        ret = tk.Label(root, text="Account was successfully closed!")
+        ret.grid()
         
     elif sure == 'no':
-        return "You chose not to close your account!"
+        ret=tk.Label(root, text="You chose not to close your account!")
+        ret.grid()
+
+def enter_info_for_close():
+    label = tk.Label(root, text= "Closing Account-")
+    sure_label=tk.Label(root, text="Are you sure you want to close your account?", font=('calibre',10, 'normal'))
+    sure_entry=tk.Entry(root, textvariable = sure_var, font = ('calibre',10,'normal'))
+    username_label = tk.Label(root, text = 'Username', font=('calibre',10, 'normal'))
+    username_entry=tk.Entry(root, textvariable = username_var, font = ('calibre',10,'normal'))
+    passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'normal'))
+    passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
     
+    sub_btn=tk.Button(root,text = 'Submit', command = close)
 
-def modify():
-    what = input("What would you like to modify in your account(name, username, or password)? ").lower()
-    user = input("Please enter your username: ")
-    password1 = input("Please enter your password: ")
-    sql1 = 'SELECT * FROM Customer_Details'
-    cur.execute(sql1)
-    rows = cur.fetchall()
-    result = any(user in tu for tu in rows)
-    result1 = any(password1 in tu for tu in rows)
-    con.commit()
-    if result == True and result1 == True:
-        if what == 'name':
-            new = input("What would you like to change the name to? ")
-            command = 'UPDATE Customer_Details SET Name = %s WHERE Username = %s'
-            val = (new, user)
-            cur.execute(command, val)
-            con.commit()
-        if what == 'username':
-            new1 = input("What would you like to change the username to? ")
-            command = 'UPDATE Customer_Details SET Username = %s WHERE Username = %s'
-            val = (new1, user)
-            cur.execute(command, val)
-            con.commit()
-        if what == 'password':
-            new = input("What would you like to change the password to? ")
-            command = 'UPDATE Customer_Details SET password = %s WHERE Username = %s'
-            val = (new, user)
-            cur.execute(command, val)
-            con.commit()
-        else:
-            print("The choice you entered is not correct!")
-    else:
-        print("Something went wrong! Please make sure you entered your Username and Password correctly.")
+    label.grid()
+    sure_label.grid()
+    sure_entry.grid()
+    username_label.grid()
+    username_entry.grid()
+    passw_label.grid()
+    passw_entry.grid()
+    
+    sub_btn.grid()
+
+def exit():
+    last = tk.Label(root, text="Thank you for coming!")
+    last.grid()
+    exit
 
 
+print(menu())
 
-play = True
 
-while play == True:
-    print(menu())
-    choice = int(input("Enter your choice: "))
-    if choice == 1:
-        create_account() 
-        print(menu)
-    elif choice == 2:
-        login()
-        print(menu)
-    elif choice == 3:
-        close()
-        print(menu)
-    elif choice == 4:
-        modify()
-        print(menu)
-    elif choice == 5:
-        print("Thank you for visiting!")
-        play = False
-        exit
-
+root.mainloop()
 cur.close()
 con.close()
